17bea97c0bd582aec7a7c2f8990a590ccb94ac0c
FIANERD <mattiasson.sofia@gmail.com>
2021-03-17 19:18:38 +0100

    AddGameInfoSite

---
+++ Controllers/GameInfoController.cs
@@ -0,0 +1,24 @@
+﻿using FreakyGame.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FreakyGame.Controllers
+{
+    public class GameInfoController : Controller
+    {
+        private readonly FreakyGameContext context;
+
+        public GameInfoController(FreakyGameContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult GameInfo()
+        {
+            var games = context.Games.ToList();
+
+            return View(games);
+        }
+    }
+}
+
