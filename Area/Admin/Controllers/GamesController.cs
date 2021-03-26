1b364f6a79957574deb45b79a4b6ffa62fd4d64d
FIANERD <mattiasson.sofia@gmail.com>
2021-03-26 22:35:13 +0100

    Set up area

---
+++ Area/Admin/Controllers/GamesController.cs
@@ -0,0 +1,151 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FreakyGame.Data;
+using FreakyGame.Data.Entities;
+
+namespace FreakyGame.Area.Admin
+{
+    [Area("Admin")]
+    public class GamesController : Controller
+    {
+        private readonly FreakyGameContext context;
+
+        public GamesController(FreakyGameContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Games.ToListAsync());
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await context.Games
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Genre,ReleaseYear,ImageUrl,UrlSlug")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(game);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await context.Games.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Genre,ReleaseYear,ImageUrl,UrlSlug")] Game game)
+        {
+            if (id != game.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(game);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GameExists(game.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await context.Games
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var game = await context.Games.FindAsync(id);
+            context.Games.Remove(game);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GameExists(int id)
+        {
+            return context.Games.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Area/Admin/Models/ViewModels/CreateGameViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreakyGame.Areas.Admin.Models.ViewModels
+{
+    public class CreateGameViewModel
+    {
+        public int Id { get; protected set; }
+
+        [Required]
+        public string Title { get; protected set; }
+
+        [Required]
+        public string Description { get; protected set; }
+
+        public string Genre { get; protected set; }
+
+        [Required]
+        public string ReleaseYear { get; set; }
+
+        [Required]
+        public Uri ImageUrl { get; protected set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UrlSlug { get; protected set; }
+
+
+        public List<SelectListItem> ListGames { get; set; }
+    }
+}
